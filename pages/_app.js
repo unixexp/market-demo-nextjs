a0f8cc6a936a63c7d0a1bfb978559028cc1653f4
Yurii Rohoza <unixexp@gmail.com>
2022-09-23 17:34:34 +0300

    add link and logging

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
 
+    console.log("aaa");
+
     return (
         <>
             <Head>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 
 import Layout from "../components/layout/layout";
 import Header from "../components/header/header";
+import Link from "next/link";
 
 export default function IndexPage() {
 
@@ -11,6 +12,9 @@ export default function IndexPage() {
                 <title>Music market</title>
             </Head>
             <Header />
+            <Link href="/products/guitars/acoustic">
+                <a>Acoustic guitars</a>
+            </Link>
         </Layout>
     );
 
